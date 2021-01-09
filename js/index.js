cd706597eb423f3baf81dbd66f51e207a79fbf29
matheus souza <ruivorobado@gmail.com>
2021-01-09 16:51:09 -0300

    novo script sendo feito

--- js/index.js
+++ js/index.js
@@ -1,61 +1,32 @@
-var hora = 0
-var minutos = 0
-var segundos = 0
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
 
-ss = document.querySelector('.horas')
-mm = document.querySelector('.minutos')
-hh = document.querySelector('.segundos')
 
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+let segundos = 0
 
-
-var timer = setInterval(() => {
-    segundos++
-    if (segundos == 61) {
-        segundos = 0
-        minutos++
-    }
-
-    if (minutos == 61) {
-        minutos = 0
-        hora++
-    }
-
-    ss.innerHTML = segundos
-    mm.innerHTML = minutos
-    hh.innerHTML = hora
-}, 1000);
-
-function start() {
-    var timer = setInterval(() => {
+function iniciaRelogio() {
+    const timer = setInterval(() => {
         segundos++
-        if (segundos == 61) {
-            segundos = 0
-            minutos++
-        }
-
-        if (minutos == 61) {
-            minutos = 0
-            hora++
-        }
-
-        ss.innerHTML = segundos
-        mm.innerHTML = minutos
-        hh.innerHTML = hora
-    }, 1000);
-
+        relogio.innerHTML = criaHoraDosSegundos(segundos)
+    }, 1000)
 }
 
-function pause() {
-    clearInterval(timer)
-}
 
-function stops() {
-    hora = 0
-    minutos = 0
-    segundos = 0
-    clearInterval(timer)
-    ss.innerHTML = segundos
-    mm.innerHTML = minutos
-    hh.innerHTML = hora
-
-}
+iniciar.addEventListener('click', function(event) {
+    iniciaRelogio()
+})
+pausar.addEventListener('click', function(event) {
+    alert('eae')
+})
+zerar.addEventListener('click', function(event) {
+    alert("cu")
+})
